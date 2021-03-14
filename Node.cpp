c486ac8ac5a133d79a945786428879753854e65e
jam-dos <s3825891@student.rmit.edu.au>
2021-03-14 13:07:12 +0400

    complete the getters and setters methods of Node.cpp file

--- Node.cpp
+++ Node.cpp
@@ -2,34 +2,32 @@
 #include <iostream>
 
 
-Node::Node(int row, int col, int dist_traveled)
-{
+Node::Node(int row, int col, int dist_traveled) {
     // TODO
 }
 
-Node::~Node(){
+Node::~Node() {
     // TODO
 }
 
-int Node::getRow(){
+int Node::getRow() {
+    return row;
+}
+
+int Node::getCol() {
+    return col;
+}
+
+int Node::getDistanceTraveled() {
+    return dist_traveled;
+}
+
+void Node::setDistanceTraveled(int dist_traveled) {
+    (*this).dist_traveled = dist_traveled;
+}
+
+int Node::getEstimatedDist2Goal(Node* goal) {
     // TODO
 }
 
-int Node::getCol(){
-    // TODO
-}
-
-int Node::getDistanceTraveled(){
-    // TODO
-}
-
-void Node::setDistanceTraveled(int dist_traveled)
-{
-    // TODO
-}
-
-int Node::getEstimatedDist2Goal(Node* goal){
-    // TODO
-}
-    
 //--------------------------------                             
